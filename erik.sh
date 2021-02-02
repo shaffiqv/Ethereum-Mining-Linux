9eb156aa988ac873e966bfdda9a913fa79cf29a7
Shafiq Vinales <shafiq.sohairi@gmail.com>
2021-02-02 18:16:52 +0800

    Binance Add

--- erik.sh
+++ erik.sh
@@ -1,2 +1,2 @@
 #bash
-./resno -U -P stratum1+tcp://0xDfd32e0D3D15727a8Ca96f6dA9C49c7aD9552685@asia1.ethermine.org:4444
+./resno -U -P stratum1+tcp://0x0fd31365382eec778ebe3f307f5de49770ee2e51@asia1.ethermine.org:4444
